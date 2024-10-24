909e7b0c402d9b6be3a7b85318090b586d117f63
Juan <jmrgjuan@gmail.com>
2024-10-24 13:17:07 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -13,7 +13,7 @@ return function ($context) {
     $client
         ->setEndpoint('https://cloud.appwrite.io/v1')
         ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
-        ->setKey($context->req->headers['x-appwrite-key']);
+        ->setKey('standard_3637a48ddd85834bc1f93cacf485a962ff32ea0ac2f3c2e26e662d3812d2fbd8335294a6f77ff7f5883d03164ed1f8261fd88095d6277caa2a9812bf765c5cf6cad4484b34f0e16911fb9d2b1047327d420a96e16dfff6d4c7e78e0ccf3d608aa9c7dc2f088db34137d074f319222352ac6208a82d7bb0bef9396331cb41c8c7');
     $users = new Users($client);
 
     try {
